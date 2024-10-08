64ee8094a83c6ee5e92a488754bf2b2c1cce863c
TommasoLencioni <tommaso.lencioni@pagopa.it>
2024-10-08 12:09:15 +0200

    refactor common data loading

--- src/conf/configuration.py
+++ src/conf/configuration.py
@@ -21,17 +21,15 @@ try:
     if settings.TARGET_ENV in all_secrets:
         secrets = all_secrets[settings.TARGET_ENV]
 except AttributeError as e:
-    logging.warning(e)
+    logging.error(e)
     exit()
-
 
 commondata = Dynaconf(
     settings_files=['commondata.yaml'],
 )
 
 try:
-    if commondata is not None:
-        print('commondata is populated')
-except AttributeError as e:
-    logging.warning(e)
+    commondata = commondata['TEST_DATA'].to_dict()
+except KeyError as e:
+    logging.error(e)
     exit()
