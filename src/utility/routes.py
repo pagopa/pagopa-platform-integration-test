2b32d5ec46aca54a8e6563ef4ed3100a46ef1211
Rares Gheorghe <rares.gheorghe@bvtech.com>
2024-10-09 12:33:43 +0200

    feat: [PQ-190] correction in commondata.yaml and routes.py.

--- src/utility/routes.py
+++ src/utility/routes.py
@@ -26,47 +26,28 @@ def get_primitive_url(context, primitive):
 
 # The method permits to retrieve the REST URL starting from action
 def get_rest_url(context, action):
-    # services = context.config.userdata.get("services")
     match action.lower():
 
         case "redirect":
-            # service_data = services.get("wisp-converter")
             service_data = context.settings.services.wisp_converter
-            # return service_data['url'] + "/payments?idSession=", ""
             return service_data.url + "/payments?idSession=", ""
 
         case "search_in_re_by_iuv":
-            # service_data = services.get("technical-support")
             service_data = context.settings.services.technical_support
-            # url = service_data['url'] + "/organizations/{creditor_institution}/iuv/{iuv}?dateFrom={date_from}&dateTo={date_to}"
             url = service_data.url + "/organizations/{creditor_institution}/iuv/{iuv}?dateFrom={date_from}&dateTo={date_to}"
             return url, context.secrets.TECHNICAL_SUPPORT_SUBSCRIPTION_KEY
 
         case "get_paymentposition_by_iuv":
-            # service_data = services.get("gpd-core")
             service_data = context.settings.services.gpd_core
-            # url = service_data['url'] + "/organizations/{creditor_institution}/paymentoptions/{iuv}/debtposition"
             url = service_data.url + "/organizations/{creditor_institution}/paymentoptions/{iuv}/debtposition"
             return url, context.secrets.GPD_SUBSCRIPTION_KEY
 
         case "create_paymentposition_and_publish":
-            # service_data = services.get("gpd-core")
             service_data = context.settings.services.gpd_core
-
-            # url = service_data['url'] + "/organizations/{creditor_institution}/debtpositions?toPublish=true"
             url = service_data.url + "/organizations/{creditor_institution}/debtpositions?toPublish=true"
             return url, context.secrets.GPD_SUBSCRIPTION_KEY
 
         case "create_paymentposition":
-            # service_data = services.get("gpd-core")
             service_data = context.settings.services.gpd_core
-
-            # url = service_data['url'] + "/organizations/{creditor_institution}/debtpositions?toPublish=false"
             url = service_data.url + "/organizations/{creditor_institution}/debtpositions?toPublish=false"
             return url, context.secrets.GPD_SUBSCRIPTION_KEY
-
-
-# class ResponseType(Enum):
-#     XML = 1
-#     JSON = 2
-#     HTML = 3
