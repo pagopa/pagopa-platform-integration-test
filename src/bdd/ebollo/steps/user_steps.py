5285a991999346078f5a774e326adddf34adf449
TommasoLencioni <tommaso.lencioni@pagopa.it>
2025-01-23 18:17:47 +0100

    feat: [PQ-296] add user step to pay mbd on checkout

--- src/bdd/ebollo/steps/user_steps.py
+++ src/bdd/ebollo/steps/user_steps.py
@@ -1,42 +1,15 @@
-import time
-
-import pytest
-from playwright.sync_api import sync_playwright
-
 from src.conf.configuration import secrets
-
-card_number = str(secrets.card_number)
-
-@pytest.fixture(scope='session')
-def playwright_browser():
-    with sync_playwright() as p:
-        browser = p.chromium.launch(headless=False)
-        yield browser
-        browser.close()
+from src.utility.ebollo.api import post_mbd
+from src.utility.ebollo.ui import mbd_checkout_payment
+from src.utility.ebollo.utils import generate_mbd_payload
 
 
-@pytest.fixture
-def page(playwright_browser):
-    page = playwright_browser.new_page()
-    yield page
-    page.close()
-
-
-def test_rtp_form_submission(page):
-    page_url = 'https://api.uat.platform.pagopa.it/ecommerce/checkout/v1/carts/xxx/redirect?clientId=CHECKOUT_CART'
-    page.goto(page_url)
-
-    page.wait_for_selector('#confirmEmail')
-    page.click('#confirmEmail')
-    page.type('#confirmEmail', 'test@test.it')
-
-    page.wait_for_selector('#paymentEmailPageButtonContinue')
-    page.click('#paymentEmailPageButtonContinue')
-
-    page.get_by_text('Carta di credito o debito').click()
-
-    time.sleep(5)
-
-    page.frame_locator('#frame_CARD_NUMBER').get_by_placeholder('0000 0000 0000').fill(card_number)
-
-    time.sleep(10000)
+def user_pays_mbd_ok_checkout(context):
+    payload = generate_mbd_payload()
+    url = context.settings.services['mbd']['url'] + str(context.secrets.organization_id) + \
+          context.settings.services['mbd']['path']
+    res = post_mbd(url=url, api_key=context.secrets.MDB_SUBSCRIPTION_KEY, mbd_payload=payload)
+    assert res.status_code == 200
+    # assert res.status_code == 201
+    mbd_checkout_payment(page=context.page, checkout_url=res.json()['checkoutRedirectUrl'], mbd_data=payload,
+                         card_info=secrets.card_info)
