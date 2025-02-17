b788ff065e9ae233340c606b4cc17dd6c544c0be
Rares Gheorghe <rares.gheorghe@bvtech.com>
2025-02-17 14:27:04 +0100

    feat: [PQ-304] added form gpd repository the integration tests

---
+++ src/bdd/gpd/step_definitions/support/clients/gpd_client.js
@@ -0,0 +1,239 @@
+const { get, post, del, put, patch } = require("../utility/axios_common");
+const fs = require("fs");
+
+const GPD_HOST = process.env.gpd_host;
+const GPD_HOST_V2 = process.env.gpd_host_v2;
+const GPD_EXTERNAL_HOST_V3 = process.env.gpd_external_host_v3;
+const GPD_API_MASSIVE = (orgId) => process.env.gpd_api_massive.replace('{orgId}', orgId);
+const API_TIMEOUT = process.env.api_timeout;
+
+const GPD_EXTERNAL_HOST = process.env.gpd_external_host;
+
+function gpdHealthCheck() {
+    return get(GPD_EXTERNAL_HOST_V3 + `/info`, {
+        headers: {
+            "Ocp-Apim-Subscription-Key": process.env.API_SUBSCRIPTION_KEY
+        }
+    })
+}
+
+function createDebtPosition(orgId, body, segCodes, toPublish = false){
+	const params = {}
+	if (segCodes) {params.segregationCodes = segCodes}
+    if (toPublish) {params.toPublish = toPublish}
+    return post(GPD_EXTERNAL_HOST_V3 + `/organizations/${orgId}/debtpositions`, body, {
+        timeout: API_TIMEOUT,
+        params,
+        headers: {
+            "Ocp-Apim-Subscription-Key": process.env.API_SUBSCRIPTION_KEY,
+            "Content-Type": "application/json"
+        }
+    })    
+}
+
+function createMassiveDebtPositions(orgId, body, segCodes){
+	const params = {}
+	if (segCodes) {params.segregationCodes = segCodes}
+    return post(GPD_HOST_V2 + GPD_API_MASSIVE(orgId), body, {
+        timeout: API_TIMEOUT,
+        params,
+        headers: {
+            "Ocp-Apim-Subscription-Key": process.env.API_SUBSCRIPTION_KEY,
+            "Content-Type": "application/json"
+        }
+    })
+}
+
+function updateDebtPosition(orgId, iupd, body, segCodes) {
+	const params = {}
+	if (segCodes) {params.segregationCodes = segCodes}
+    return put(GPD_EXTERNAL_HOST_V3 + `/organizations/${orgId}/debtpositions/${iupd}`, body, {
+        timeout: API_TIMEOUT,
+        params,
+        headers: {
+            "Ocp-Apim-Subscription-Key": process.env.API_SUBSCRIPTION_KEY,
+            "Content-Type": "application/json"
+        }
+    })
+}
+
+function publishDebtPosition(orgId, iupd, segCodes) {
+	const params = {}
+	if (segCodes) {params.segregationCodes = segCodes}
+    return post(GPD_EXTERNAL_HOST_V3 + `/organizations/${orgId}/debtpositions/${iupd}/publish`, "", {
+        timeout: API_TIMEOUT,
+        params,
+        headers: {
+            "Ocp-Apim-Subscription-Key": process.env.API_SUBSCRIPTION_KEY,
+            "Content-Type": "application/json"
+        }
+    })
+}
+
+function invalidateDebtPosition(orgId, iupd, segCodes) {
+	const params = {}
+	if (segCodes) {params.segregationCodes = segCodes}
+    return post(GPD_EXTERNAL_HOST_V3 + `/organizations/${orgId}/debtpositions/${iupd}/invalidate`, "", {
+        timeout: API_TIMEOUT,
+        params,
+        headers: {
+            "Ocp-Apim-Subscription-Key": process.env.API_SUBSCRIPTION_KEY,
+            "Content-Type": "application/json"
+        }
+    })
+}
+
+function getDebtPositionList(orgId, dueDateFrom, dueDateTo, paymentDateFrom, paymentDateTo, status, segCodes) {
+    const params = {}
+    if (dueDateFrom != null) params.due_date_from = dueDateFrom;
+    if (dueDateTo != null) params.due_date_to = dueDateTo;
+    if (paymentDateFrom != null) params.payment_date_from = paymentDateFrom;
+    if (paymentDateTo != null) params.payment_date_to = paymentDateTo;
+    if (status != null) params.status = status;
+    if (segCodes) {params.segregationCodes = segCodes}
+    params.orderby = "INSERTED_DATE";
+    params.ordering = "ASC";
+    params.page = 0;
+
+    return get(GPD_EXTERNAL_HOST + `/organizations/${orgId}/debtpositions`, {
+        timeout: API_TIMEOUT,
+        params,
+        headers: {
+            "Ocp-Apim-Subscription-Key": process.env.API_SUBSCRIPTION_KEY,
+            "Content-Type": "application/json"
+        }
+    })
+}
+
+function getDebtPosition(orgId, iupd, segCodes) {
+	const params = {}
+	if (segCodes) {params.segregationCodes = segCodes}
+    return get(GPD_EXTERNAL_HOST_V3 + `/organizations/${orgId}/debtpositions/${iupd}`, {
+        timeout: API_TIMEOUT,
+        headers: {
+            "Ocp-Apim-Subscription-Key": process.env.API_SUBSCRIPTION_KEY,
+            "Content-Type": "application/json"
+        }
+    })
+}
+
+function getDebtPositionByIUV(orgId, iuv, segCodes) {
+    const params = {}
+    if (segCodes) {params.segregationCodes = segCodes}
+    return get(GPD_EXTERNAL_HOST_V3 + `/organizations/${orgId}/paymentoptions/${iuv}/debtposition`, {
+        timeout: API_TIMEOUT,
+        headers: {
+            "Ocp-Apim-Subscription-Key": process.env.API_SUBSCRIPTION_KEY,
+            "Content-Type": "application/json"
+        }
+    })
+}
+
+function getPaymentOptionByIuv(orgId, iuv) {
+    return get(GPD_EXTERNAL_HOST_V3 + `/organizations/${orgId}/paymentoptions/${iuv}`, {
+        timeout: API_TIMEOUT,
+        headers: {
+            "Ocp-Apim-Subscription-Key": process.env.API_SUBSCRIPTION_KEY,
+            "Content-Type": "application/json"
+        }
+    })
+}
+
+function deleteDebtPosition(orgId, iupd, segCodes) {
+	const params = {}
+	if (segCodes) {params.segregationCodes = segCodes}
+    return del(GPD_EXTERNAL_HOST_V3 + `/organizations/${orgId}/debtpositions/${iupd}`, {
+        timeout: API_TIMEOUT,
+        params,
+        headers: {
+            "Ocp-Apim-Subscription-Key": process.env.API_SUBSCRIPTION_KEY,
+            "Content-Type": "application/json"
+        }
+    })
+}
+
+function payPaymentOption(orgId, iuv, body) {
+    return post(GPD_EXTERNAL_HOST_V3 + `/organizations/${orgId}/paymentoptions/${iuv}/pay`, body, {
+        timeout: API_TIMEOUT,
+        headers: {
+            "Ocp-Apim-Subscription-Key": process.env.API_SUBSCRIPTION_KEY,
+            "Content-Type": "application/json"
+        }
+    })
+}
+
+function reportTransfer(orgId, iuv, idTransfer) {
+    return post(GPD_EXTERNAL_HOST_V3 + `/organizations/${orgId}/paymentoptions/${iuv}/transfers/${idTransfer}/report`, "", {
+        timeout: API_TIMEOUT,
+        headers: {
+            "Ocp-Apim-Subscription-Key": process.env.API_SUBSCRIPTION_KEY,
+            "Content-Type": "application/json"
+        }
+    })
+}
+
+function updateNotificationFee(orgId, iuv, body) {
+    return put(GPD_EXTERNAL_HOST_V3 + `/organizations/${orgId}/paymentoptions/${iuv}/notificationfee`, body, {
+        timeout: API_TIMEOUT,
+        headers: {
+            "Ocp-Apim-Subscription-Key": process.env.API_SUBSCRIPTION_KEY,
+            "Content-Type": "application/json"
+        }
+    })
+}
+
+function createAndPublishDebtPosition(orgId, body) {
+    return post(GPD_EXTERNAL_HOST_V3 + `/organizations/${orgId}/debtpositions`, body, {
+        timeout: API_TIMEOUT,
+        params: {
+            toPublish: "True",
+        },
+        headers: {
+            "Ocp-Apim-Subscription-Key": process.env.API_SUBSCRIPTION_KEY,
+            "Content-Type": "application/json"
+        }
+    })
+}
+
+function updateAndPublishDebtPosition(orgId, iupd, body) {
+    return put(GPD_EXTERNAL_HOST_V3 + `/organizations/${orgId}/debtpositions/${iupd}`, body, {
+        timeout: API_TIMEOUT,
+        params: {
+            toPublish: "True",
+        },
+        headers: {
+            "Ocp-Apim-Subscription-Key": process.env.API_SUBSCRIPTION_KEY,
+            "Content-Type": "application/json"
+        }
+    })
+}
+
+function updateTransferIbanMassive(orgId, oldIban, newIban) {
+    return patch(GPD_EXTERNAL_HOST_V3 + `/organizations/${orgId}/debtpositions/transfers?oldIban=${oldIban}&limit=10`, {newIban}, {
+        timeout: API_TIMEOUT,
+        headers: {
+            "Ocp-Apim-Subscription-Key": process.env.API_SUBSCRIPTION_KEY,
+            "Content-Type": "application/json"
+        }
+    })
+}
+
+module.exports = {
+    gpdHealthCheck,
+    createDebtPosition,
+    publishDebtPosition,
+    updateDebtPosition,
+    updateNotificationFee,
+    getDebtPositionList,
+    getDebtPosition,
+    getDebtPositionByIUV,
+    deleteDebtPosition,
+    payPaymentOption,
+    reportTransfer,
+    createAndPublishDebtPosition,
+    updateAndPublishDebtPosition,
+    getPaymentOptionByIuv,
+    invalidateDebtPosition,
+    createMassiveDebtPositions,
+    updateTransferIbanMassive
+}
---
+++ src/bdd/gpd/step_definitions/support/utility/request_builders.js
@@ -0,0 +1,292 @@
+const { addDays, buildStringFromDate, makeidNumber, makeidMix,  } = require("./helpers");
+
+function buildDebtPositionDynamicData(gpdSessionBundle, iupdIn, iuv = makeidNumber(17), validityDate = null) {
+    return {
+        iupd: iupdIn,
+        iuv1: iuv,
+        iuv2: makeidNumber(17),
+        iuv3: makeidNumber(17),
+        iuvOK: process.env.iuv_ok,  // es. "11101751670642134"
+        iuvKO: process.env.iuv_ko,  // es. "03163674189686371"
+        iban: gpdSessionBundle.debtPosition.iban,
+        validityDate: validityDate,
+        dueDate: addDays(30),
+        retentionDate: addDays(90),
+        transferId1: '1',
+        transferId2: '2',
+        amount: 300.00,
+        receiptId: makeidMix(33),
+        pspId: "60000000001",
+        pspBrokerId: "60000000001",
+        pspChannelId: "60000000001_01",
+        pspName: "PSP Paolo",
+        pspFiscalCode: "CF60000000006",
+        idempotency: `60000000001_${makeidNumber(6)}${makeidMix(4)}`,
+        applicationDate: buildStringFromDate(addDays(0)),
+        transferDate: buildStringFromDate(addDays(1)),
+        transferOtherCIFiscalCode: "01020304059"
+    };
+}
+
+function buildCreateDebtPositionRequest(debtPosition, payer) {
+    return {
+        iupd: debtPosition.iupd,
+        type: "F",
+        fiscalCode: payer.fiscalCode,
+        fullName: payer.name,
+        streetName: payer.streetName,
+        civicNumber: payer.civicNumber,
+        postalCode: payer.postalCode,
+        city: payer.city,
+        province: payer.province,
+        region: payer.region,
+        country: payer.country,
+        email: payer.email,
+        phone: payer.phone,
+        companyName: payer.companyName,
+        officeName: payer.officeName,
+        validityDate: debtPosition.validityDate,
+        switchToExpired: false,
+        paymentOption: [
+            {
+                iuv: debtPosition.iuv1,
+                amount: debtPosition.amount * 100,
+                description: "Canone Unico Patrimoniale - SkyLab Inc.",
+                isPartialPayment: false,
+                dueDate: debtPosition.dueDate,
+                retentionDate: debtPosition.retentionDate,
+                fee: 0,
+                transfer: [
+                    {
+                        idTransfer: debtPosition.transferId1,
+                        amount: (debtPosition.amount * 100 / 3),
+                        remittanceInformation: "Rata 1",
+                        category: "9/0101108TS/",
+                        iban: debtPosition.iban,
+                    },
+                    {
+                        idTransfer: debtPosition.transferId2,
+                        organizationFiscalCode: debtPosition.transferOtherCIFiscalCode,
+                        amount: (debtPosition.amount * 100 / 3) * 2,
+                        remittanceInformation: "Rata 2",
+                        category: "9/0101108TS/",
+                        iban: debtPosition.iban,
+                    }
+                ]
+            }
+        ]
+    };
+}
+
+function buildCreateMassiveDebtPositionRequest(debtPosition, payer) {
+	
+	return {
+			  paymentPositions: [
+			    {
+			        iupd: debtPosition.iupd,
+			        type: "F",
+			        fiscalCode: payer.fiscalCode,
+			        fullName: payer.name,
+			        streetName: payer.streetName,
+			        civicNumber: payer.civicNumber,
+			        postalCode: payer.postalCode,
+			        city: payer.city,
+			        province: payer.province,
+			        region: payer.region,
+			        country: payer.country,
+			        email: payer.email,
+			        phone: payer.phone,
+			        companyName: payer.companyName,
+			        officeName: payer.officeName,
+			        switchToExpired: false,
+			        paymentOption: [
+			            {
+			                iuv: debtPosition.iuv1,
+			                amount: debtPosition.amount * 100,
+			                description: "Canone Unico Patrimoniale - SkyLab Inc.",
+			                isPartialPayment: false,
+			                dueDate: debtPosition.dueDate,
+			                retentionDate: debtPosition.retentionDate,
+			                fee: 0,
+			                transfer: [
+			                    {
+			                        idTransfer: debtPosition.transferId1,
+			                        amount: (debtPosition.amount * 100 / 3),
+			                        remittanceInformation: "Rata 1",
+			                        category: "9/0101108TS/",
+			                        iban: debtPosition.iban,
+			                    },
+			                    {
+			                        idTransfer: debtPosition.transferId2,
+			                        organizationFiscalCode: debtPosition.transferOtherCIFiscalCode,
+			                        amount: (debtPosition.amount * 100 / 3) * 2,
+			                        remittanceInformation: "Rata 2",
+			                        category: "9/0101108TS/",
+			                        iban: debtPosition.iban,
+			                    }
+			                ],
+			                paymentOptionMetadata: [{key: "string",value: "string"}]
+			            }
+			      ]
+			   }
+			 ]
+		};
+}
+
+function buildCreateOK_KODebtPositionRequest(bundle, action) {
+	let debtPosition = bundle.debtPosition;
+	let payer = bundle.payer;
+    return {
+        iupd: debtPosition.iupd,
+        type: "F",
+        fiscalCode: payer.fiscalCode,
+        fullName: payer.name,
+        streetName: payer.streetName,
+        civicNumber: payer.civicNumber,
+        postalCode: payer.postalCode,
+        city: payer.city,
+        province: payer.province,
+        region: payer.region,
+        country: payer.country,
+        email: payer.email,
+        phone: payer.phone,
+        companyName: payer.companyName,
+        officeName: payer.officeName,
+        switchToExpired: false,
+        paymentOption: [
+            {
+                iuv: action == "OK" ? debtPosition.iuvOK:debtPosition.iuvKO,
+                amount: debtPosition.amount * 100,
+                description: "Canone Unico Patrimoniale - SkyLab Inc.",
+                isPartialPayment: false,
+                dueDate: debtPosition.dueDate,
+                retentionDate: debtPosition.retentionDate,
+                fee: 0,
+                transfer: [
+                    {
+                        idTransfer: debtPosition.transferId1,
+                        amount: (debtPosition.amount * 100 / 3),
+                        remittanceInformation: "Rata 1",
+                        category: "9/0101108TS/",
+                        iban: debtPosition.iban,
+                    },
+                    {
+                        idTransfer: debtPosition.transferId2,
+                        organizationFiscalCode: bundle.organizationCode,
+                        amount: (debtPosition.amount * 100 / 3) * 2,
+                        remittanceInformation: "Rata 2",
+                        category: "9/0101108TS/",
+                        iban: debtPosition.iban,
+                    }
+                ]
+            }
+        ]
+    };
+}
+
+function buildUpdateDebtPositionRequest(debtPosition, payer) {
+    return {
+        iupd: debtPosition.iupd,
+        type: "F",
+        fiscalCode: payer.fiscalCode,
+        fullName: payer.name,
+        streetName: payer.streetName,
+        civicNumber: payer.civicNumber,
+        postalCode: payer.postalCode,
+        city: payer.city,
+        province: payer.province,
+        region: payer.region,
+        country: payer.country,
+        email: payer.email,
+        phone: payer.phone,
+        companyName: payer.companyName + " - Edit",
+        officeName: payer.officeName + " - Edit",
+        switchToExpired: false,
+        paymentOption: [
+            {
+                iuv: debtPosition.iuv1,
+                amount: debtPosition.amount * 100,
+                description: "Canone Unico Patrimoniale - SkyLab Inc. - Edit",
+                isPartialPayment: false,
+                dueDate: debtPosition.dueDate,
+                retentionDate: debtPosition.retentionDate,
+                fee: 0,
+                transfer: [
+                    {
+                        idTransfer: debtPosition.transferId1,
+                        amount: (debtPosition.amount * 100 / 3),
+                        remittanceInformation: "Rata 1 Edit",
+                        category: "9/0101108TS/",
+                        iban: debtPosition.iban,
+                    },
+                    {
+                        idTransfer: debtPosition.transferId2,
+                        organizationFiscalCode: debtPosition.transferOtherCIFiscalCode,
+                        amount: (debtPosition.amount * 100 / 3) * 2,
+                        remittanceInformation: "Rata 2 Edit",
+                        category: "9/0101108TS/",
+                        iban: debtPosition.iban,
+                    }
+                ]
+            }
+        ]
+    };
+}
+
+function buildUpdateDebtPositionInfoRequest(debtPosition, payer) {
+    return {
+        iupd: debtPosition.iupd,
+        type: "F",
+        fiscalCode: payer.fiscalCode,
+        fullName: payer.fullName,
+        streetName: payer.streetName,
+        civicNumber: payer.civicNumber,
+        postalCode: payer.postalCode,
+        city: payer.city,
+        province: payer.province,
+        region: payer.region,
+        country: payer.country,
+        email: payer.email,
+        phone: payer.phone,
+        companyName: payer.companyName,
+        officeName: payer.officeName,
+        switchToExpired: false,
+        paymentOption: [
+            {
+                iuv: debtPosition.paymentOption[0].iuv,
+                amount: debtPosition.paymentOption[0].amount * 100,
+                description: "Canone Unico Patrimoniale - SkyLab Inc. - Edit",
+                isPartialPayment: false,
+                dueDate: debtPosition.paymentOption[0].dueDate,
+                retentionDate: debtPosition.paymentOption[0].retentionDate,
+                fee: 0,
+                transfer: [
+                    {
+                        idTransfer: debtPosition.paymentOption[0].transfer[0].idTransfer,
+                        amount: (debtPosition.paymentOption[0].amount * 100 / 3),
+                        remittanceInformation: "Rata 1 Edit",
+                        category: "9/0101108TS/",
+                        iban: debtPosition.paymentOption[0].transfer[0].iban,
+                    },
+                    {
+                        idTransfer: debtPosition.paymentOption[0].transfer[1].idTransfer,
+                        organizationFiscalCode: debtPosition.transferOtherCIFiscalCode,
+                        amount: (debtPosition.paymentOption[0].amount * 100 / 3) * 2,
+                        remittanceInformation: "Rata 2 Edit",
+                        category: "9/0101108TS/",
+                        iban: debtPosition.paymentOption[0].transfer[1].iban,
+                    }
+                ]
+            }
+        ]
+    };
+}
+
+module.exports = {
+    buildCreateDebtPositionRequest,
+    buildUpdateDebtPositionRequest,
+    buildDebtPositionDynamicData,
+    buildUpdateDebtPositionInfoRequest,
+    buildCreateOK_KODebtPositionRequest,
+    buildCreateMassiveDebtPositionRequest
+}
