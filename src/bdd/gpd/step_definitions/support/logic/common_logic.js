8db7684282a8ea4c49aa2a979f6a97219758d867
rgBvtech <rares.gheorghe@bvtech.com>
2025-02-20 16:28:20 +0100

    feat: [PQ-304] add integration tests form gpd repository (#23)
    * feat: [PQ-304] added form gpd repository the integration tests
    
    * feat: [PQ-304] added form gpd repository the integration tests
    
    * gitignore update
    
    * feat: [PQ-304] modified methods from v1 to v3
    
    * feat: [PQ-304] v1 version of pagopa-debt-position branch
    
    * feat: [PQ-304] v1 corrections
    
    * feat: [PQ-304] correction in gitignore
    
    * feat: [PQ-304] gitignore and env changes

---
+++ src/bdd/gpd/step_definitions/support/logic/common_logic.js
@@ -0,0 +1,93 @@
+const assert = require("assert");
+
+const auxDigit = process.env.aux_digit
+
+async function assertAmount(bundle, amount) {
+    await assertOutcome(bundle, "OK");
+    await assert.match(bundle.responseToCheck.data, new RegExp(`<amount>${amount}</amount>`, "g"));
+}
+
+async function assertFaultCode(bundle, fault) {
+    await assertOutcome(bundle, "KO");
+    await assert.match(bundle.responseToCheck.data, new RegExp(`<faultCode>${fault}</faultCode>`, "g"));
+}
+
+async function assertOutcome(bundle, outcome) {
+    assert.match(bundle.responseToCheck.data, new RegExp(`<outcome>${outcome}</outcome>`, "g"));
+}
+
+async function assertStatusCode(bundle, statusCode) {
+    assert.strictEqual(bundle.responseToCheck.status, statusCode);
+}
+
+async function assertCompanyName(bundle, companyName) {
+    assert.strictEqual(bundle.payer.companyName, companyName);
+}
+
+async function assertDebtPositionStatus(bundle, debtPositionStatus) {
+    assert.strictEqual(bundle.debtPositionStatus, debtPositionStatus);
+}
+
+async function assertIupd(bundle) {
+  assert.strictEqual(bundle.debtPosition.iupd, bundle.responseToCheck.data.iupd)
+}
+
+async function assertStatusString(bundle, statusString) {
+    assert.strictEqual(bundle.responseToCheck.data.status, statusString);
+}
+
+async function assertNotificationFeeUpdatedAmounts(createdDebtPosition, response) {
+    let notificationFee = response.notificationFee;
+    assert.strictEqual(response.amount, createdDebtPosition.paymentOption[0].amount + notificationFee);
+    assert.strictEqual(response.transfer[0].amount, createdDebtPosition.paymentOption[0].transfer[0].amount + notificationFee);
+    assert.strictEqual(response.transfer[1].amount, createdDebtPosition.paymentOption[0].transfer[1].amount);
+}
+
+async function assertNotificationFeeUpdatedDateNotificationFee(createdDebtPosition, response) {
+	let lastUpdatedDateNotificationFee = response.lastUpdatedDateNotificationFee;
+	assert.notEqual(lastUpdatedDateNotificationFee, null);
+}
+
+async function assertNav(debtPosition, response) {
+    // nav = auxDigit + iuv
+    assert.strictEqual(response.paymentOption[0].nav, auxDigit + debtPosition.paymentOption[0].iuv);
+}
+
+async function assertIUV(response, expectedIUV) {
+    assert.strictEqual(response.paymentOption[0].iuv, expectedIUV);
+}
+
+async function assertSize (array, expectedSize){
+	assert.strictEqual(array.length, expectedSize)
+}
+
+async function assertMinSize (array, minSize){
+	assert.ok(array.length >= minSize)
+}
+
+function randomOrg() {
+    return "Org_" + Math.floor(Math.random() * 100);
+}
+
+function randomIupd() {
+    return "IUPD_" + Math.floor(Math.random() * 1000000);
+}
+
+module.exports = {
+    assertAmount,
+    assertFaultCode,
+    assertOutcome,
+    assertStatusCode,
+    assertCompanyName,
+    assertDebtPositionStatus,
+    assertNotificationFeeUpdatedAmounts,
+    assertStatusString,
+    randomOrg,
+    randomIupd,
+    assertIupd,
+    assertNav,
+    assertIUV,
+    assertNotificationFeeUpdatedDateNotificationFee,
+    assertSize,
+    assertMinSize
+}
---
+++ src/bdd/gpd/step_definitions/support/logic/gpd_logic.js
@@ -0,0 +1,238 @@
+const {
+    createDebtPosition,
+    updateDebtPosition,
+    updateNotificationFee,
+    publishDebtPosition,
+    deleteDebtPosition,
+    getDebtPositionList,
+    getDebtPosition,
+    payPaymentOption,
+    reportTransfer,
+    createAndPublishDebtPosition,
+    updateAndPublishDebtPosition,
+    getPaymentOptionByIuv,
+    invalidateDebtPosition,
+    createMassiveDebtPositions,
+    getDebtPositionByIUV,
+    updateTransferIbanMassive
+} = require("../clients/gpd_client");
+
+const {
+    buildDebtPositionDynamicData,
+    buildCreateDebtPositionRequest,
+    buildUpdateDebtPositionRequest,
+    buildUpdateDebtPositionInfoRequest,
+    buildCreateOK_KODebtPositionRequest,
+    buildCreateMassiveDebtPositionRequest
+} = require("../utility/request_builders");
+
+async function executeDebtPositionCreation(bundle, idOrg, iupd, iuv, validityDate = null, toPublish = false) {
+    bundle.organizationCode = idOrg;
+    bundle.debtPosition = buildDebtPositionDynamicData(bundle, iupd, iuv, validityDate);
+    let response = await createDebtPosition(bundle.organizationCode, buildCreateDebtPositionRequest(bundle.debtPosition, bundle.payer), segCodes = undefined, toPublish = toPublish);
+    bundle.responseToCheck = response;
+    bundle.createdDebtPosition = bundle.responseToCheck.data;
+}
+
+async function executeMassiveDebtPositionsCreation(bundle, idOrg, iupd) {
+    bundle.organizationCode = idOrg;
+    bundle.debtPosition = buildDebtPositionDynamicData(bundle, iupd);
+    let response = await createMassiveDebtPositions(bundle.organizationCode, buildCreateMassiveDebtPositionRequest(bundle.debtPosition, bundle.payer));
+    bundle.responseToCheck = response;
+}
+
+async function executeDebtPositionCreationWithSegregationCodes(bundle, idOrg, iupd) {
+    bundle.organizationCode = idOrg;
+    bundle.debtPosition = buildDebtPositionDynamicData(bundle, iupd);
+    let segCodes = bundle.debtPosition.iuv1.slice(0, 2)
+    let response = await createDebtPosition(bundle.organizationCode, buildCreateDebtPositionRequest(bundle.debtPosition, bundle.payer), segCodes);
+    bundle.responseToCheck = response;
+    bundle.createdDebtPosition = bundle.responseToCheck.data;
+}
+
+async function executeMassiveDebtPositionCreationWithSegregationCodes (bundle, idOrg, iupd) {
+	bundle.organizationCode = idOrg;
+    bundle.debtPosition = buildDebtPositionDynamicData(bundle, iupd);
+    let segCodes = bundle.debtPosition.iuv1.slice(0, 2)
+    let response = await createMassiveDebtPositions(bundle.organizationCode, buildCreateMassiveDebtPositionRequest(bundle.debtPosition, bundle.payer), segCodes);
+    bundle.responseToCheck = response;
+}
+
+async function executeOKDebtPositionCreation(bundle, idOrg, iupd) {
+    bundle.organizationCode = idOrg;
+    bundle.debtPosition = buildDebtPositionDynamicData(bundle, iupd);
+    let response = await createDebtPosition(bundle.organizationCode, buildCreateOK_KODebtPositionRequest(bundle, "OK"));
+    bundle.responseToCheck = response;
+    bundle.createdDebtPosition = bundle.responseToCheck.data;
+}
+
+async function executeKODebtPositionCreation(bundle, idOrg, iupd) {
+    bundle.organizationCode = idOrg;
+    bundle.debtPosition = buildDebtPositionDynamicData(bundle, iupd);
+    let response = await createDebtPosition(bundle.organizationCode, buildCreateOK_KODebtPositionRequest(bundle, "KO"));
+    bundle.responseToCheck = response;
+    bundle.createdDebtPosition = bundle.responseToCheck.data;
+}
+
+async function executeDebtPositionUpdate(bundle, payer, idOrg, iupd) {
+	let updatedDebtPosition=buildUpdateDebtPositionInfoRequest(bundle.createdDebtPosition, payer)
+    updateDebtPosition.iupd = iupd;
+    let response = await updateDebtPosition(idOrg, iupd, updatedDebtPosition);
+    bundle.responseToCheck = response;
+    bundle.updatedDebtPosition = bundle.responseToCheck.data;
+}
+
+async function executeDebtPositionUpdateWithSegregationCodes(bundle, payer, idOrg, iupd) {
+    let updatedDebtPosition=buildUpdateDebtPositionInfoRequest(bundle.createdDebtPosition, payer)
+    updateDebtPosition.iupd = iupd;
+    let segCodes = updatedDebtPosition.paymentOption[0].iuv.slice(0, 2)
+    let response = await updateDebtPosition(idOrg, iupd, updatedDebtPosition, segCodes);
+    bundle.responseToCheck = response;
+    bundle.updatedDebtPosition = bundle.responseToCheck.data;
+}
+
+async function executeDebtPositionNotificationFeeUpdate(bundle, idOrg, fee) {
+    let iuv = bundle.debtPosition.iuv1;
+    let response = await updateNotificationFee(idOrg, iuv, {notificationFee: fee});
+    bundle.responseToCheck = response;    
+}
+
+async function executeDebtPositionNotificationFeeUpdateNodeOK(bundle, idOrg, fee) {
+    let iuv = bundle.debtPosition.iuvOK;
+    let response = await updateNotificationFee(idOrg, iuv, {notificationFee: fee});
+    bundle.responseToCheck = response;    
+}
+
+async function executeDebtPositionNotificationFeeUpdateNodeKO(bundle, idOrg, fee) {
+    let iuv = bundle.debtPosition.iuvKO;
+    let response = await updateNotificationFee(idOrg, iuv, {notificationFee: fee});
+    bundle.responseToCheck = response;    
+}
+
+async function executeDebtPositionGetList(bundle, idOrg, dueDateFrom, dueDateTo, paymentDateFrom, paymentDateTo, status) {
+    let response = await getDebtPositionList(idOrg, dueDateFrom, dueDateTo, paymentDateFrom, paymentDateTo, status);
+    bundle.responseToCheck = response;
+}
+
+async function executeDebtPositionGetListWithSegregationCodes(bundle, idOrg){
+	let segCodes = bundle.debtPosition.iuv1.slice(0, 2)
+	let response = await getDebtPositionList(idOrg, null, null, null, null, null, segCodes);
+	bundle.responseToCheck = response;
+}
+
+async function executeDebtPositionGet(bundle, idOrg, iupd) {
+    let response = await getDebtPosition(idOrg, iupd);
+    bundle.payer.companyName = response.data.companyName;
+    bundle.responseToCheck = response;
+}
+
+async function executeDebtPositionGetByIuv(bundle, idOrg, iuv) {
+    let response = await getDebtPositionByIUV(idOrg, iuv);
+    bundle.payer.companyName = response.data.companyName;
+    bundle.responseToCheck = response;
+}
+
+async function executeDebtPositionGetWithSegregationCodes(bundle, idOrg, iupd) {
+	let segCodes = bundle.debtPosition.iuv1.slice(0, 2)
+    let response = await getDebtPosition(idOrg, iupd, segCodes);
+    bundle.payer.companyName = response.data.companyName;
+    bundle.responseToCheck = response;
+}
+
+async function executePaymentOptionGetByIuv(bundle, idOrg, iuv) {
+    let response = await getPaymentOptionByIuv(idOrg, iuv);
+    bundle.responseToCheck = response;
+}
+
+async function executeDebtPositionDeletion(bundle, idOrg, iupd) {
+    let response = await deleteDebtPosition(idOrg, iupd);
+    bundle.responseToCheck = response;
+}
+
+async function executeDebtPositionDeletionWithSegregationCodes(bundle, idOrg, iupd) {
+	let segCodes = bundle.debtPosition.iuv1.slice(0, 2)
+    let response = await deleteDebtPosition(idOrg, iupd, segCodes);
+    bundle.responseToCheck = response;
+}
+
+async function executeDebtPositionPublish(bundle, idOrg, iupd) {
+    delete bundle.responseToCheck;
+    let response = await publishDebtPosition(idOrg, iupd);
+    bundle.responseToCheck = response;
+}
+
+async function executeDebtPositionPublishWithSegregationCodes(bundle, idOrg, iupd) {
+    delete bundle.responseToCheck;
+    let segCodes = bundle.debtPosition.iuv1.slice(0, 2)
+    let response = await publishDebtPosition(idOrg, iupd, segCodes);
+    bundle.responseToCheck = response;
+}
+
+async function executeDebtPositionInvalidateWithSegregationCodes(bundle, idOrg, iupd) {
+    delete bundle.responseToCheck;
+    let segCodes = bundle.debtPosition.iuv1.slice(0, 2)
+    let response = await invalidateDebtPosition(idOrg, iupd, segCodes);
+    bundle.responseToCheck = response;
+}
+
+async function executePaymentOptionPay(bundle, idOrg, iuv) {
+    bundle.paymentDate = new Date();
+    let response = await payPaymentOption(idOrg, iuv, bundle);
+    bundle.responseToCheck = response;
+}
+
+async function executeReportTransfer(bundle, idOrg) {
+    let iuv = bundle.debtPosition.iuv1;
+    let idTransfer = '1';
+    let response = await reportTransfer(idOrg, iuv, idTransfer);
+}
+
+async function executeDebtPositionCreationAndPublication(bundle, idOrg, iupd) {
+    bundle.organizationCode = idOrg;
+    bundle.debtPosition = buildDebtPositionDynamicData(bundle, iupd);
+    let response = await createAndPublishDebtPosition(bundle.organizationCode, buildUpdateDebtPositionRequest(bundle.debtPosition, bundle.payer));
+    console.log("Response from createAndPublishDebtPosition:", response);
+
+    bundle.responseToCheck = response;
+}
+
+async function executeDebtPositionUpdateAndPublication(bundle, idOrg, iupd) {
+    bundle.organizationCode = idOrg;
+    let response = await updateAndPublishDebtPosition(bundle.organizationCode, iupd, buildCreateDebtPositionRequest(bundle.debtPosition, bundle.payer));
+    bundle.responseToCheck = response;
+}
+
+async function executeUpdateTransferIbanMassive(idOrg, oldIban, newIban) {
+    let response = await updateTransferIbanMassive(idOrg, oldIban, newIban);
+    return response;
+}
+
+module.exports = {
+    executeDebtPositionCreation,
+    executeDebtPositionDeletion,
+    executeDebtPositionGetList,
+    executeDebtPositionUpdate,
+    executeDebtPositionNotificationFeeUpdate,
+    executeDebtPositionGet,
+    executeDebtPositionGetByIuv,
+    executeDebtPositionPublish,
+    executePaymentOptionPay,
+    executeReportTransfer,
+    executeDebtPositionCreationAndPublication,
+    executeDebtPositionUpdateAndPublication,
+    executePaymentOptionGetByIuv,
+    executeKODebtPositionCreation,
+    executeOKDebtPositionCreation,
+    executeDebtPositionNotificationFeeUpdateNodeOK,
+    executeDebtPositionNotificationFeeUpdateNodeKO,
+    executeDebtPositionCreationWithSegregationCodes,
+    executeDebtPositionUpdateWithSegregationCodes,
+    executeDebtPositionGetWithSegregationCodes,
+    executeDebtPositionGetListWithSegregationCodes,
+    executeDebtPositionDeletionWithSegregationCodes,
+    executeDebtPositionPublishWithSegregationCodes,
+    executeDebtPositionInvalidateWithSegregationCodes,
+    executeMassiveDebtPositionsCreation,
+    executeMassiveDebtPositionCreationWithSegregationCodes,
+    executeUpdateTransferIbanMassive
+}
