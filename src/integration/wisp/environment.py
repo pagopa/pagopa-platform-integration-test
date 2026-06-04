6a71c2f40942f539bbd7b3a32a5132cd8344dbb6
daniele-quero <danio.quero@gmail.com>
2026-06-04 14:49:37 +0200

    fix: introducing scalable delay in after_scenario function to try and overcome flaky tests failing in UAT

--- src/integration/wisp/environment.py
+++ src/integration/wisp/environment.py
@@ -1,11 +1,16 @@
 import copy
 import logging
+import time
 
 from src.conf.configuration import commondata
 from src.conf.configuration import secrets
 from src.conf.configuration import settings
 from src.integration.utility.wisp import constants
 from src.integration.utility.wisp.constants import empty_flow_data
+
+# Delay between scenarios (seconds) to reduce backend contention on UAT.
+# Set to 0 to disable. Override via -D scenario_delay=<seconds> at runtime.
+_DEFAULT_SCENARIO_DELAY = 5
 
 
 def before_all(context):
@@ -17,10 +22,17 @@ def before_all(context):
     # configure logging setup
     logging.basicConfig(level=logging.DEBUG)
 
+    # scenario delay: allow override via -D scenario_delay=<seconds>
+    context.scenario_delay = int(context.config.userdata.get('scenario_delay', _DEFAULT_SCENARIO_DELAY))
+
 
 def before_scenario(context, scenario):
     context.flow_data = copy.deepcopy(empty_flow_data)
     context.flow_data['action']['trigger_primitive']['name'] = constants.PRIMITIVE_NODOINVIARPT
+
+def after_scenario(context, scenario):
+    if context.scenario_delay > 0:
+        time.sleep(context.scenario_delay)
 
 def before_step(context, step):
     context.running_step = step.name
