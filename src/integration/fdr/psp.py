6d7a9eaa51ba2a9900424f3b77d05c57159b245d
marcopiccoloalten-hash <marco.piccolo.alten@gmail.com>
2026-07-29 15:46:33 +0200

    PA-545: pspinfo endpoint

--- src/integration/fdr/psp.py
+++ src/integration/fdr/psp.py
@@ -6,7 +6,7 @@ def get_psp_info(context, psp_id):
         context: Il contesto di Behave.
         psp_id: L'ID del PSP da recuperare.
     """
-    psp_info_endpoint = f"{context.config.psp_base_url}/paymentserviceproviders/{psp_id}"
+    psp_info_endpoint = f"/paymentserviceproviders/{psp_id}"
     context.get_psp_response = context.psp.rest.client.get(psp_info_endpoint)
 
 # https://api.uat.platform.pagopa.it/apiconfig/auth/api/v1/paymentserviceproviders/WOLLNLB1
